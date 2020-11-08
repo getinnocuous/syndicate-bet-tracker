ea0896ebb73709b6362e33fbd1fb7a0912977534
Elliot Waddington <elliot.waddington@autotrader.co.uk>
2020-11-08 16:01:16 +0000

    only show in play section is games are playing

--- src/components/Stage/Stage.tsx
+++ src/components/Stage/Stage.tsx
@@ -11,47 +11,55 @@ interface StageProps {
   };
 }
 
-export const Stage = ({ scores }: StageProps): JSX.Element => (
-  <main>
-    <h2 className="h1">In Play</h2>
-    <Column>
-      <>
-        <h2 className={'winning'}>Winning</h2>
-        <FixtureList scores={scores.inPlayOrPending.filter((score) => score.betStatus === BetStatus.Winning)} />
-      </>
-    </Column>
-    <Column>
-      <>
-        <h2 className={'losing'}>Losing</h2>
-        <FixtureList
-          scores={scores.inPlayOrPending.filter(
-            (score) => score.betStatus !== BetStatus.Winning && score.betStatus !== BetStatus.Pending
-          )}
-        />
-      </>
-    </Column>
-    <h2 className="h1">Settled</h2>
-    <Column>
-      <>
-        <h2 className={'winning'}>Won</h2>
-        <FixtureList scores={scores.finishedGames.filter((score) => score.betStatus === BetStatus.Winning)} />
-      </>
-    </Column>
-    <Column>
-      <>
-        <h2 className={'losing'}>Lost</h2>
-        <FixtureList
-          scores={scores.finishedGames.filter(
-            (score) => score.betStatus !== BetStatus.Winning && score.betStatus !== BetStatus.Pending
-          )}
-        />
-      </>
-    </Column>
-    <Centered>
-      <h2 className={'drawing'}>Pending</h2>
-      <Grid>
-        <FixtureList scores={scores.inPlayOrPending.filter((score) => score.betStatus === BetStatus.Pending)} />
-      </Grid>
-    </Centered>
-  </main>
-);
+export const Stage = ({ scores }: StageProps): JSX.Element => {
+  const gamesInPlay = scores.inPlayOrPending.some((score) => score.betStatus !== BetStatus.Pending);
+  console.log({ gamesInPlay });
+  return (
+    <main>
+      {gamesInPlay ? (
+        <>
+          <h2 className="h1">In Play</h2>
+          <Column>
+            <>
+              <h2 className={'winning'}>Winning</h2>
+              <FixtureList scores={scores.inPlayOrPending.filter((score) => score.betStatus === BetStatus.Winning)} />
+            </>
+          </Column>
+          <Column>
+            <>
+              <h2 className={'losing'}>Losing</h2>
+              <FixtureList
+                scores={scores.inPlayOrPending.filter(
+                  (score) => score.betStatus !== BetStatus.Winning && score.betStatus !== BetStatus.Pending
+                )}
+              />
+            </>
+          </Column>
+        </>
+      ) : null}
+      <h2 className="h1">Settled</h2>
+      <Column>
+        <>
+          <h2 className={'winning'}>Won</h2>
+          <FixtureList scores={scores.finishedGames.filter((score) => score.betStatus === BetStatus.Winning)} />
+        </>
+      </Column>
+      <Column>
+        <>
+          <h2 className={'losing'}>Lost</h2>
+          <FixtureList
+            scores={scores.finishedGames.filter(
+              (score) => score.betStatus !== BetStatus.Winning && score.betStatus !== BetStatus.Pending
+            )}
+          />
+        </>
+      </Column>
+      <Centered>
+        <h2 className={'drawing'}>Pending</h2>
+        <Grid>
+          <FixtureList scores={scores.inPlayOrPending.filter((score) => score.betStatus === BetStatus.Pending)} />
+        </Grid>
+      </Centered>
+    </main>
+  );
+};
