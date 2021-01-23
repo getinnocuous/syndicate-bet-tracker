4679a0eff513f442baf0b53296b51925e2150f18
Elliot Waddington <elliot.waddington@cinch.co.uk>
2021-01-23 20:09:44 +0000

    turning off

--- scraper/src/app.ts
+++ scraper/src/app.ts
@@ -16,8 +16,8 @@ appRouter(app, fs);
 
 const server = app.listen(3001, () => {
   // Schedule tasks to be run on the server.
-  scrapeScores().then(() => console.log('SCRAPED AT LAUNCH'));
-  cron.schedule('*/30 * * * * *', function () {
-    scrapeScores().then(() => console.log(`SCRAPED AT ${prettyTime()}`));
-  });
+  // scrapeScores().then(() => console.log('SCRAPED AT LAUNCH'));
+  // cron.schedule('*/30 * * * * *', function () {
+  //   scrapeScores().then(() => console.log(`SCRAPED AT ${prettyTime()}`));
+  // });
 });
